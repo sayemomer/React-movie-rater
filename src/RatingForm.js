5244be6b2e64ae90e37ffb2c8ede6b9c3468d9f5
omersayem <omer_sayem@yahoo.com>
2017-11-08 19:44:48 +0600

    commentBox functionality added

--- src/RatingForm.js
+++ src/RatingForm.js
@@ -3,17 +3,58 @@ import React from "react";
 class RatingForm extends React.Component{
   constructor(props){
     super(props);
+    this.state={
+      characters : 0 ,
+      errorMessage:" "
+    }
 
     this.newComment=this.newComment.bind(this);
 
   }  
+
+  characterCount(){
+    if( this.comment.value.length <100 ){
+      this.setState({
+        errorMessage:" "
+      })
+    }
+    else{
+      this.setState({
+        errorMessage:" Max value exceeds "
+  
+      })
+
+    }
+    this.setState({
+      characters : this.comment.value.length 
+    })
+
+
+  }
   
  
 
   newComment(){
-    this.props.addComment(this.email.value,this.comment.value);
-    this.email.value="";
-    this.comment.value="";
+    if ( this.email.value ==0 || this.comment.value==0 ){
+      this.setState({
+        errorMessage:" Can't submit empty "
+
+      })
+
+
+    }
+    else if ( !this.state.errorMessage==""){
+      this.props.addComment(this.email.value,this.comment.value);
+      
+          this.email.value="";
+          this.comment.value="";
+          this.setState({
+            errorMessage:" "
+          })
+          this.setState({ characters: 0 });
+
+    }
+   
   }
 
   render(){
@@ -30,8 +71,11 @@ class RatingForm extends React.Component{
       <div class="field">
       <label class="label">Comment</label>
       <div class="control">
-        <textarea class="textarea" placeholder="Textarea"  ref={ (c)=> this.comment =c}></textarea>
-        
+
+        <textarea class="textarea" placeholder="Comment"  ref={ (c)=> this.comment =c} onKeyUp={this.characterCount.bind(this)}></textarea>
+
+        <p> {this.state.characters} characters (Max 100)  </p>
+        <p>{this.state.errorMessage}</p>
         <button class="button is-primary" onClick={this.newComment}>Submit</button>
     
       </div>
